eac549021397188c6423db7822d418e8d4716e72
jun <pickjog@gmail.com>
2025-12-04 15:03:42 +0900

    first commit

---
+++ src/main/java/io/github/junhyeong9812/overload/OverloadApplication.java
@@ -0,0 +1,13 @@
+package io.github.junhyeong9812.overload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/junhyeong9812/overload/OverloadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.junhyeong9812.overload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
